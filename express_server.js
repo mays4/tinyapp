24df337af0203946057c6acc50656134f7a8550d
mays4 <maysalani84@yahoo.com>
2021-09-27 13:03:49 -0400

    changes in codes

--- express_server.js
+++ express_server.js
@@ -7,8 +7,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
